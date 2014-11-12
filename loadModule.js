90a8ee53a7d563f366716a9441b746cbfccd2e69
yemol <yemol_yuan@hotmail.com>
2014-11-13 00:59:50 +0800

    Update to add notes to case

--- loadModule.js
+++ loadModule.js
@@ -1,3 +1,7 @@
+//使用require调用自己的模块。默认的模块的入口方法是export
+//我们可以使用函数作为参数进行传递
+//数组foreach遍历数组执行foreach中的方法
+
 var myModule = require("./mymodule.js");
 var dir = process.argv[2];
 var filterstr = process.argv[3];
--- mymodule.js
+++ mymodule.js
@@ -1,21 +1,19 @@
 //module.exports -- 暴露module的默认方法
+//list.filter －－ 通过数组的filter方法对数组进行过滤，可以使用一个返回布尔值的方法作为参数
+//path.extname -- 取得某一个文件或者路径＋文件的后缀名
 
-module.exports = function(dir, filter, callback)
+module.exports ＝ function(dir, filter, callback)
 {
   var fs = require('fs');
+  var path = require('path');
 
   fs.readdir(dir, function(err, list) {
     if (err) return callback(err);
 
-    var fileList = Array();
-    for(i = 0; i < list.length; i++)
-    {
-      if (list[i].indexOf(filter) > 0)
-      {
-        fileList.push(list[i]);
-      }
-    }
-    callback(null,fileList);
+    list = list.filter(function(file){
+      return path.extname(file) == "." + filter;
+    });
+    callback(null,list);
   });
 
 }
